fafdf03d513b32de3c6221bdff55060098ce9fa0
Azamat_Abidokov <azamat_abidokov@epam.com>
2018-12-18 11:58:22 +0300

    Update UI, it became more comfortable

--- js/calc.js
+++ js/calc.js
@@ -1,45 +1,32 @@
 $(document).ready(function () {
 
-    $('#lab1_btn_calc').click(function () {
-        var inputs = checkInputs("lab1_r_input", "lab1_m_input", "lab1_time_input");
+    $('#btn_calc').click(function () {
+        var inputs = checkInputs("r_input", "m_input", "time_input");
 
         var l_r = inputs.l_r;
         var l_m = inputs.l_m;
         var t = inputs.t;
 
-        var P = 1 - (1 - Math.exp(-l_r * t))
+        var reservationMethod = $('#reservation-method input:radio:checked').val();
+        var P = 0.0;
+        if (reservationMethod === '0') {
+        P = 1 - (1 - Math.exp(-l_r * t))
             * (1 - Math.exp(-l_m * t))
             * (1 - Math.exp(-l_m * t) * Math.exp(-l_m * t))
             * (1 - Math.exp(-l_m * t) * Math.exp(-l_m * t) * Math.exp(-l_m * t));
-        document.getElementById("lab1_result").value = P.toFixed(20);
-    });
+        } else if (reservationMethod === '1') {
+            P = 1 - (1 - (3 * Math.exp(-2 * l_r * t) - 2 * Math.exp(-3 * l_r * t)))
+                * (1 - Math.exp(-l_m * t))
+                * (1 - Math.exp(-l_m * t) * Math.exp(-l_m * t))
+                * (1 - Math.exp(-l_m * t) * Math.exp(-l_m * t) * Math.exp(-l_m * t));
+        } else if(reservationMethod === '2') {
+            P = 1 - (1 - (2 * Math.exp(-l_r * t) - Math.exp(-2 * l_r * t)))
+                * (1 - Math.exp(-l_m * t))
+                * (1 - Math.exp(-l_m * t) * Math.exp(-l_m * t))
+                * (1 - Math.exp(-l_m * t) * Math.exp(-l_m * t) * Math.exp(-l_m * t));
+        }
 
-    $('#lab2A_btn_calc').click(function () {
-        var inputs = checkInputs("lab2A_r_input", "lab2A_m_input", "lab2A_time_input");
-
-        var l_r = inputs.l_r;
-        var l_m = inputs.l_m;
-        var t = inputs.t;
-
-        var P = 1 - (1 - (3 * Math.exp(-2 * l_r * t) - 2 * Math.exp(-3 * l_r * t)))
-            * (1 - Math.exp(-l_m * t))
-            * (1 - Math.exp(-l_m * t) * Math.exp(-l_m * t))
-            * (1 - Math.exp(-l_m * t) * Math.exp(-l_m * t) * Math.exp(-l_m * t));
-        document.getElementById("lab2A_result").value = P.toFixed(20);
-    });
-
-    $('#lab2B_btn_calc').click(function () {
-        var inputs = checkInputs("lab2B_r_input", "lab2B_m_input", "lab2B_time_input");
-
-        var l_r = inputs.l_r;
-        var l_m = inputs.l_m;
-        var t = inputs.t;
-
-        var P = 1 - (1 - (2 * Math.exp(-l_r * t) - Math.exp(-2 * l_r * t)))
-            * (1 - Math.exp(-l_m * t))
-            * (1 - Math.exp(-l_m * t) * Math.exp(-l_m * t))
-            * (1 - Math.exp(-l_m * t) * Math.exp(-l_m * t) * Math.exp(-l_m * t));
-        document.getElementById("lab2B_result").value = P.toFixed(20);
+        document.getElementById("result").value = P.toFixed(20);
     });
 
     function checkInputs(rInputId, mInputId, timeInputId) {
